cc0b1cd33ca4b72f56ffddb0485c580c54062bdc
NicolaBellini <bellininicola4366@gmail.com>
2024-03-08 16:08:23 +0100

    [fix] es 4 bundle 3

--- bundle1/main.js
+++ bundle1/main.js
@@ -52,7 +52,7 @@ loopToFive();
 function displayEvenNumbers() {
     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
     let evenNumbers = [];
-    for (let i = 0; i < numbers.length-1; i++) {
+    for (let i = 0; i < numbers.length; i++) {
         if ((numbers[i] % 2)===0) {
             evenNumbers.push(numbers[i]);
         }
