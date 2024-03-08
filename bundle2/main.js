2080c35f6153659078105b41f5a4dd2f9f441590
NicolaBellini <bellininicola4366@gmail.com>
2024-03-08 15:52:19 +0100

    [fix] bundle 2

--- bundle2/main.js
+++ bundle2/main.js
@@ -12,44 +12,71 @@
 
 // ESERCIZIO 1
 function checkAge() {
-    const myAge = 32;
-    const message = '';
+    const myAge = 16;
+    let message = '';
 
     if (myAge < 18) {
-        message = `Sei troppo giovane! Hai ${myAge} anni!`;
+        return message = `Sei troppo giovane! Hai ${myAge} anni!`;
     } else {
-        message = 'Hai più di 18 anni!';
+        return message = 'Hai più di 18 anni!';
     }
+    console.log(message);
 }
 checkAge();
+console.log(checkAge());
+
+// funziona
+// ho cambiato da const a let
+
+
 
 // ESERCIZIO 2
 function printColorsNumber() {
     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
-    console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
+    const lunghezza = colors.length
+    console.log(`Nella mia palette ci sono ${lunghezza} colori!`);
+    console.log(colors.length);
 }
 printColorsNumber();
 
+// funziona?
+// ho assegnato la lunghezza dell' array ad una variabile
+
+
+
+
+
 
 // ESERCIZIO 3
-function addNumbers() {
-    const userNumber = prompt('Inserisci un numero');
-    const total = userNumber + 12;
 
-    console.log(`Il risultato finale è ${total}`);
-}
-addNumbers();
+// let total = 0;
+
+// function addNumbers() {
+//     // const userNumber =parseInt (prompt('Inserisci un numero'));
+//     let total = userNumber + 12;
+
+//     console.log(`Il risultato finale è ${total}`);
+// }
+// addNumbers();
+
+
+// serviva un parse int del prompt, poichè se no essendo un input ti restituisce una stringa, che sommata a un numero non da il risultato corretto
+
+
+
 
 
 // ESERCIZIO 4
+
 function checkAccess() {
     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
-    const userEmail = prompt('Inserisci il tuo indirizzo email');
+    const userEmail =  prompt('Inserisci il tuo indirizzo email');
 
-    let grantAccess = 'false';
+
+    let grantAccess = false;
 
     if (addresses.includes(userEmail)) {
-        grantAccess = 'true';
+        grantAccess = true;
     }
 
     if (grantAccess === true) {
@@ -59,6 +86,7 @@ function checkAccess() {
     }
 }
 checkAccess();
+// i valori booleani tre e false del flag erano stringhe
 
 
 // ESERCIZIO 5 (suggerimento: c'è un solo errore)
@@ -87,9 +115,12 @@ function checkAccessImproved() {
             console.log('Accesso negato!');
         }
     }
-    checkAccessImproved();
+}
+checkAccessImproved();
+console.log(checkAccessImproved());
 
-
+// nonn era stat invocata la funzione
+// messo } 
 
 
 
